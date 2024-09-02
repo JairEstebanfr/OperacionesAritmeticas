b19c45524f736eaed02a67f0b32510c6f4c050d8
JairEstebanfr <70582220@continental.edu.pe>
2024-09-02 18:47:18 -0500

    Proyecto de suma V1_2 EN POO

--- Suma_V1_1.py
+++ Suma_V1_1.py
@@ -7,5 +7,5 @@ def suma(numero1,numero2):
     return numero1+numero2
 
 if __name__== '__main__':
-num1,num2 = IngresoNumeros()
-print(f"{num1} + {num2} = {suma(num1,num2)}")
+     num1, num2 = IngresoNumeros()
+    print(f"{num1} + {num2} = {suma(num1,num2)}")
